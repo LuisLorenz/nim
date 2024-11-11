d9a2e7a182832d7ba15c0fc0151a52cf3ff6ba48
Luis Lorenzs <luislorenz7@gmail.com>
2024-11-11 19:44:26 +0100

    working on nim

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ## principle 
+- pile with items to take 
+    - e.g.: amount of 10 
+- the player that takes the last item loses the game 
+- AI player caculates always the best move so that he never loses 
+- ever round a player must take 1-3 itmes 
 
 ## plan 
 -[ ] 
--- nim_game.py
+++ nim_game.py
@@ -1,4 +1,9 @@
+# player
+    # is_maximizing -> computer player
+    # is minimizing -> human best player 
+
 def minimax(pile, is_maximizing):
+    # this func returns the max/min score
     # check for empty pile 
         # pile == 0, player = X
         # player x is winner because player y took the last item the turn before
@@ -12,12 +17,37 @@ def minimax(pile, is_maximizing):
             # min = 1
             # max = 3 =< plie 
             scores = [minimax(pile - take, is_maximizing=False)]
-            return max(scores)
+        return max(scores)
     else:
-        score = 0 
+        score = 0 # or '- inf'
         for take in range(1, min(pile, 3) + 1):
             # min = 1
             # max = 3 =< plie 
             minimax(pile - take, is_maximizing=True)
+        return min(scores)
 
-    pass 
+def move(pile, is_maximizing): 
+    if is_maximizing: 
+        # minimax ...
+        pass
+    else: 
+        print(f'The current pile has {pile} items.' )
+        valid_take = False
+        while valid_take == False: 
+            take = input('How much do you want to take?: ')
+            if take in range(1, min(pile, 3) + 1):
+                return valid_take == True
+            else:
+                print('Your input was invalid. Please try again.')
+    pile = pile - take
+    if pile == 0:
+        if is_maximizing:
+            winner = 'user'
+        else: 
+            winner = 'computer'
+    # create a game loop in the game func
+
+
+
+def game():
+    pass
