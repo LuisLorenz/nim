090243c9388601bfe3e6b29d9727790383a0ef00
Luis Lorenzs <luislorenz7@gmail.com>
2024-12-11 21:15:45 +0100

    finishing flow writing implementation

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## plan 
 -[x] seeing the computer has made a move 
--[ ] implement the nice illustration of flow writing fully  
+-[x] implement the nice illustration of flow writing fully  
 -[ ] random pile choice
 -[ ] player vs player choice 
 -[ ] record counter
--- nim_game_v2.py
+++ nim_game_v2.py
@@ -29,7 +29,9 @@ def move(pile, is_maximizing):
     if is_maximizing: 
         best_move = minimax(pile, is_maximizing)
         take = best_move[1]
-        print(f'The computer has taken {take}.')
+        text_computer_take = f'The computer has taken {take} ...'
+        flow_writing(text_computer_take)
+        # print(f'The computer has taken {take}.')
         return take
     else: 
         text_current_pile = f'The current pile has {pile} items ...'
@@ -41,12 +43,16 @@ def move(pile, is_maximizing):
             take_options = []
             for x in range(1, min(pile, 3) + 1):
                 take_options.append(x)
-            take = int(input(f'Select a take: {take_options}: '))
+            text_take = f'Select a take: {take_options}: '
+            flow_writing(text_take)
+            # take = int(input(f'Select a take: {take_options}: '))
+            take = int(input('>> '))
             if take in range(1, min(pile, 3) + 1):
                 valid_take = True
                 return take
             else:
-                print('Your input was invalid. Please try again.')
+                text_invalid = 'Your input was invalid. Please try again.'
+                flow_writing(text_invalid)
 
 class flow_writing:
     def __init__(self, text):
@@ -81,21 +87,31 @@ def game():
 
     # end 
     if not is_maximizing: 
-        print("I'm sorry, you lost the game!")
+        t_loss = "I'm sorry, you lost the game!" 
+        flow_writing(t_loss)
+        # print("I'm sorry, you lost the game!")
         score_player_2 += 1 
     else:
-        print('Congrates, you won this game.')
+        t_win = 'Congrates, you won this game.'
+        flow_writing(t_win)
+        # print('Congrates, you won this game.')
         score_player_1 += 1
 
 score_player_1 = 0 
 score_player_2 = 0 
 
 def score_board():
-    print(f"""
+    t_score_board = f"""
 score board: 
   player 1: {score_player_1}
   player 2: {score_player_2}
-""")
+"""
+    flow_writing(t_score_board)
+#     print(f"""
+# score board: 
+#   player 1: {score_player_1}
+#   player 2: {score_player_2}
+# """)
 
 intro = '''
 Welcome to NIM! ...
@@ -118,7 +134,10 @@ while super_loop == True:
     score_board()
     play_again = None
     while play_again == None: 
-        play_again = input('Do you want to play again? (y/n): ')
+        text_play_again = 'Do you want to play again? (y/n): '
+        flow_writing(text_play_again) 
+        play_again = input('>> ')
+        # play_again = input('Do you want to play again? (y/n): ')
         if play_again == 'y':
             # between each game alternate the starting player 
             if is_maximizing:
@@ -129,7 +148,9 @@ while super_loop == True:
         elif play_again == 'n':
             super_loop = False
         else: 
-            print('Your input was unvalid. Please try again.')
+            text_invalid = 'Your input was unvalid. Please try again.'
+            flow_writing(text_invalid)
+            # print('Your input was unvalid. Please try again.')
             play_again = None
 
     
