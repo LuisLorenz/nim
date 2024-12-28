e720c170f35eab10c0937e98f9ec0b4ab1a4ed98
Luis Lorenzs <luislorenz7@gmail.com>
2024-12-28 13:01:55 +0100

    working on coloring

---
+++ basic_code/color_text.py
@@ -0,0 +1,27 @@
+from colorama import Fore, Back, Style
+
+print(Fore.RED + "Dieser Text ist rot!")
+print(Fore.GREEN + "Dieser Text ist grün!")
+print(Back.YELLOW + "Dieser Text hat einen gelben Hintergrund!")
+# print(Style.RESET_ALL + "Dieser Text hat den Standardstil zurück.")
+print('test')
+print(Style.RESET_ALL) #  reseting the style
+print('test')
+print(Style.DIM + "Hello")
+print(Style.BRIGHT + "Hello")
+# Hello
+# Hello
+print(Fore.BLUE + Back.YELLOW + "This is blue text on a yellow background.")
+print(Fore.RED + "Red text" + Style.RESET_ALL + " Normal text")
+print(Fore.BLUE + 'color')
+print(Fore.CYAN + 'color')
+print(Fore.GREEN + 'color')
+print(Fore.LIGHTBLACK_EX + 'color')
+print(Fore.LIGHTCYAN_EX + 'color')
+print(Fore.LIGHTBLUE_EX + 'color')
+print(Fore.LIGHTGREEN_EX + 'color')
+print(Fore.LIGHTMAGENTA_EX + 'color')
+print(Fore.LIGHTRED_EX + 'color')
+print(Fore.LIGHTWHITE_EX + 'color')
+print(Fore.LIGHTYELLOW_EX + 'color')
+print(Fore.MAGENTA + 'color')
